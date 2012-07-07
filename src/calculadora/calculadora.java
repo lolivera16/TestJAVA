89addf222e411b1001c3c138efa0413d9b223ae8
lolivera16 <oliver_aries2@hotmail.com>
2012-07-07 18:51:10 -0500

    Firs File

---
+++ src/calculadora/calculadora.java
@@ -0,0 +1,15 @@
+package calculadora;
+
+public class calculadora {
+
+	public int sumar(int i, int j) {
+		// TODO Auto-generated method stub
+		return i +j;
+	}
+		//Sobrecarga - overloading
+	public double sumar(double d, double e) {
+		// TODO Auto-generated method stub
+		return d + e;
+	}
+	
+}
